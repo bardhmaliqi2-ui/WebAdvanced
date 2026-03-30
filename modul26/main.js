1dcc913844f635b084e557126ece36c0e5219c00
bardhmaliqi2-ui <bardhmaliqi2@gmail.com>
2026-03-30 16:14:15 +0200

    30/3/2026

--- modul26/main.js
+++ modul26/main.js
@@ -1,5 +1,7 @@
 console.log('Goodbye');
+
 var x=5;
+
 var a=50+50;
 console.log(a);
 
@@ -17,3 +19,66 @@ var h=10;
 var i=4;
 var j=h%i;
 console.log(j);
+
+var k=99;
+k++;
+console.log(k);
+
+var l=113;
+l--;
+console.log(l);
+
+var p=20;
+var q=p**2;
+console.log(q);
+
+var r=15;
+r+=5;
+console.log(r);
+
+var s=15;
+s-=5;
+console.log(s);
+
+var t=15;
+t*=5;
+console.log(t);
+
+var u=15;
+u/=5;
+console.log(u);
+
+var v=5;
+var w=5;
+console.log(v==w);
+
+var y=5;
+var z=6;
+console.log(y!=z);
+
+var num1=100;
+var num2=10;
+console.log(num1>num2);
+
+var num3=2;
+var num4=1;
+console.log(num3<num4);
+
+var num5=123;
+var num6="123";
+console.log(num5===num6);
+
+var firstName="Digital";
+var lastName="School";
+var fullName=firstName+" "+lastName;
+console.log(fullName);
+
+var school="Digital School";
+console.log(`Hello from ${school}`);
+
+var age=23;
+console.log(age>=15 && age<-65);
+
+var var1=5;
+var var2=6;
+console.log(var1==5 || var2==5);
