25107c1f378930f396e464b5cea1aaef7c35136f
bardhmaliqi2-ui <bardhmaliqi2@gmail.com>
2026-05-18 16:14:06 +0200

    18/5/2026
    3:13pm

--- modul33/script.js
+++ modul33/script.js
@@ -36,4 +36,13 @@ function validation(){
         }
         return false;
     } 
+        else{
+            document.getElementById("name_error").style.visibility="hidden";
+            document.getElementById("name").style.borderColor="black";
+            document.getElementById("age_error").style.visibility="hidden";
+            document.getElementById("age").style.borderColor="black";
+            document.getElementById("city_error").style.visibility="hidden";
+            document.getElementById("city").style.borderColor="black";
+            return true;
+        }
 }
---
+++ modul34/modul34_1/script.js
@@ -0,0 +1,13 @@
+var colors=['red','green','blue','purple'];
+
+function changeBgColor(){
+    document.querySelector('body').style.background=colors[Math.floor(Math.random()*colors.length)];
+}
+
+var names=['Rion','Bardh','Reis','Nehar'];
+function changeNames(){
+    document.querySelector('p').innerHTML=names[Math.floor(Math.random()*names.length)];
+}
+setInterval(changeBgColor,1000);
+setInterval(changeNames,1000);
+
