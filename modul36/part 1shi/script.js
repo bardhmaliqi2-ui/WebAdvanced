ee5b0ef668cad4139cd29b9dba616b362d21cce2
bardhmaliqi2-ui <bardhmaliqi2@gmail.com>
2026-06-03 16:12:23 +0200

    3/6/2026
    4:12pm

---
+++ modul36/part 1shi/script.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    alert('Its working')
+})
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm:input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
